d4fcbf976ef3e9bfc4e14e684c7331741d5f93bf
AlexandruChelaru-end <Alexandru_Chelaru@endava.com>
2025-12-08 09:32:43 +0200

    Add ex3_4

---
+++ ch10ex3_4/src/main/java/com/example/demo/controllers/CountryController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*ex3
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public Country france() {
+        Country c = Country.of("France", 67);
+        return c;
+    }
+
+    @GetMapping("/all")
+    public List<Country> countries() {
+        Country c1 = Country.of("France", 67);
+        Country c2 = Country.of("Spain", 47);
+
+        return List.of(c1,c2);
+    }
+}*/
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public ResponseEntity<Country> france() {
+        Country c = Country.of("France", 67);
+
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(c);
+    }
+}
