a63f84da9b854f99a58002a8446854dcddd49116
AlexandruChelaru-end <Alexandru_Chelaru@endava.com>
2025-12-10 17:19:08 +0200

    Add ex 1

---
+++ ch14ex1/src/main/java/com/example/demo/controllers/AccountController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.TransferRequest;
+import com.example.demo.model.Account;
+import com.example.demo.services.TransferService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+
+    private final TransferService transferService;
+
+    public AccountController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public void transferMoney(
+            @RequestBody TransferRequest request
+    ) {
+        transferService.transferMoney(
+                request.getSenderAccountId(),
+                request.getReceiverAccountId(),
+                request.getAmount());
+    }
+
+    @GetMapping("/accounts")
+    public Iterable<Account> getAllAccounts(
+            @RequestParam(required = false) String name
+    ) {
+        if (name == null) {
+            return transferService.getAllAccounts();
+        } else {
+            return transferService.findAccountsByName(name);
+        }
+    }
+
+}
